c85b54cef3c38c62dd5d27bcf19b071e235565f5
Billy Shih <me@billyshih.com>
2013-05-09 12:49:53 -0700

    Setup rails 3 and gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBlog::Application.config.secret_token = 'c38ca36bac3cf0d00816653699ff537e262274a401057a64e771138582c29f43e90421f28232f7d070b5acb0c68f59a87dfebd8a4e183f484a6c7c207353b316'
